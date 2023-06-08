e5f76bd41cb2a14ff9712d4f97a6a633517db828
myspringbootstyle <108319537+myspringbootstyle@users.noreply.github.com>
2023-06-08 11:29:20 +0900

    업데이트

--- resources/web/legend/js/sidebar.js
+++ resources/web/legend/js/sidebar.js
@@ -1,4 +1,3 @@
-// Sidebar
 var sidebar_id;
 var sidebar_size = "-320px";
 
@@ -70,51 +69,13 @@ function sidebar_open(id) {
 		}
 	}
 
-	// Show
-	if(is_show) {
-		$('.sidebar-item').hide();
 
-		if(id == "sidebar-user") {
-			$('.sidebar-common').hide();
-		} else {
-			$('.sidebar-common').show();
-		}
 
-		switch(id) {
-			case 'sidebar-response'	: $('#' + id + '-list').load(sidebar_url + '/response.php'); break;
-			case 'sidebar-cart'		: $('#' + id + '-list').load(sidebar_url + '/cart.php'); break;
-		}
-
-		$('#' + id).show();
-		$('#sidebar-content').scrollTop(0);
-	}
-
-	// Save id
 	sidebar_id = id;
 
 	return false;
 }
 
-// sidebar Empty
-function sidebar_empty(id) {
-
-	// Ajax
-	switch(id) {
-		case 'sidebar-cart' : $('#' + id + '-list').load(sidebar_url + '/cart.php?del=1'); break;
-	}
-
-	return false;
-}
-
-// sidebar Read
-function sidebar_read(id) {
-
-	$('#sidebar-response-list').load(sidebar_url + '/response.php?read=1&id=' + id);
-
-	return false;
-}
-
-// sidebar Href
 function sidebar_href(url) {
 
 	$('.sidebar-menu .panel-collapse').hide();
@@ -124,35 +85,7 @@ function sidebar_href(url) {
 	return false;
 }
 
-// sidebar Login
-function sidebar_login(f) {
-	if (f.mb_id.value == '') {
-		alert('아이디를 입력해 주세요.');
-		f.mb_id.focus();
-		return false;
-	}
-	if (f.mb_password.value == '') {
-		alert('비밀번호를 입력해 주세요.');
-		f.mb_password.focus();
-		return false;
-	}
-	return true;
-}
 
-// sidebar Search
-function sidebar_search(f) {
-
-	if (f.stx.value.length < 2) {
-		alert("검색어는 두글자 이상 입력하십시오.");
-		f.stx.select();
-		f.stx.focus();
-		return false;
-	}
-
-	f.action = f.url.value;
-
-	return true;
-}
 
 // sidebar Response Count
 function sidebar_response() {
@@ -172,16 +105,9 @@ function sidebar_response() {
 	return false;
 }
 
-// Response Auto Check
-if(g5_is_member && sidebar_time > 0) {
-	setInterval(function() {
-		sidebar_response();
-	}, sidebar_time * 1000); // Time = 1000ms ex) 10sec = 10 * 1000
-}
 
 $(document).ready(function () {
 
-	// Sidebar Close
 	$('.sidebar-close').on('click', function () {
 		var div = $("#sidebar-box");
 		var side = is_sidebar();
@@ -222,5 +148,24 @@ $(document).ready(function () {
 			$("#sidebar-box").css(side, '');
 			sidebar_mask('hide');
 		}
+	});
+	
+	
+	
+   $('.menu-li .menu-a').hover(function(){
+		$(this).addClass("on-menu");
+		$('.menu-ul').addClass('on-hover');
+	},function(){
+		$(this).removeClass("on-menu");
+		$('.menu-ul').removeClass('on-hover');
+	});
+	
+	$(".nav-user a").on("mouseover", function(e){
+		$(".nav-user a").css("color", "rgba(255,255,255,.3)");
+		$(this).css("color", "#fff");
+	});
+
+	$(".nav-user a").on("mouseout", function(e){
+		$(".nav-user a").css("color", "#fff");
 	});
 });
